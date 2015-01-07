a141a0cb3b44a7e28a2cfa672a8995fa37322c27
小小小七 <junjjun990@163.com>
2015-01-07 23:57:52 +0800

    添加输出日志和单元测试

--- ki4so-java-client/src/main/java/com/github/ebnew/ki4so/client/web/filters/Ki4soLogoutFilter.java
+++ ki4so-java-client/src/main/java/com/github/ebnew/ki4so/client/web/filters/Ki4soLogoutFilter.java
@@ -10,6 +10,11 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
+import com.github.ebnew.ki4so.client.handler.AppClientLogoutHandler;
+import com.github.ebnew.ki4so.common.utils.StringUtils;
+
 /**
  * ki4so处理登出 请求的过滤器。
  * @author burgess yang
@@ -18,37 +23,67 @@ import javax.servlet.http.HttpServletResponse;
 public class Ki4soLogoutFilter extends BaseClientFilter {
 	
 	//当前应用的登出地址
-	private String currentAppLogoutUrl = "http://localhost:8080/ki4so-web/logout.do";
+	private static String currentAppLogoutUrl = "http://localhost:8080/ki4so-web/logout.do";
 	
 	//登出成功跳转地址
-	private String logoutSuccessUrl = "http://localhost:8080/ki4so-app";
+	private static String logoutSuccessUrl = "http://localhost:8080/ki4so-app";
+	
+	/**
+	 * 登录本应用处理器类，由此类进行构造一个对象。
+	 */
+	protected String appClientLogoutHandlerClass = "com.github.ebnew.ki4so.app.custom.Ki4soAppClientLogoutHandlerImpl";
+
+	Logger logger = Logger.getLogger(Ki4soLogoutFilter.class);
+	/**
+	 * 登录本应用的处理器。
+	 */
+	protected AppClientLogoutHandler appClientLogoutHandler;
 	
 	@Override
 	public void doInit(FilterConfig filterConfig) throws ServletException {
+		logger.debug("doInit begin ..");
 		//初始化参数值。
 		currentAppLogoutUrl = this.getInitParameterWithDefalutValue(filterConfig, "currentAppLogoutUrl", currentAppLogoutUrl);
 		logoutSuccessUrl = this.getInitParameterWithDefalutValue(filterConfig, "logoutSuccessUrl", logoutSuccessUrl);
+		appClientLogoutHandlerClass = getInitParameterWithDefalutValue(filterConfig, "appClientLoginHandlerClass", appClientLogoutHandlerClass);
+		//构造登录本应用的处理器对象。
+		if(!StringUtils.isEmpty(appClientLogoutHandlerClass)){
+			try{
+				this.appClientLogoutHandler = (AppClientLogoutHandler)Class.forName(appClientLogoutHandlerClass).newInstance();
+			}catch (Exception e) {
+				logger.error("Class for appClientLogoutHandler is error ",e);
+			}
+		}
+		logger.debug("doInit end ..");
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
+		logger.debug(" doFilter begin .." );
 		HttpServletResponse servletResponse = (HttpServletResponse)response;
 		HttpServletRequest servletRequest = (HttpServletRequest)request;
+//		HttpSession session = servletRequest.getSession();
+		if(appClientLogoutHandler!=null){
+			//登出本应用。
+			appClientLogoutHandler.logoutClient(servletRequest, servletResponse);
+		}
 		//若已经登录，则作相关处理。
-		servletResponse.sendRedirect(buildRedirectToKi4soServer(servletRequest));
+		servletResponse.sendRedirect(buildRedirectToKi4soServer());
+		logger.debug(" doFilter end .." );
 		return;
 	}
 	
-	protected String buildRedirectToKi4soServer(HttpServletRequest servletRequest){
-		StringBuffer sb = new StringBuffer(this.currentAppLogoutUrl);
-		if(this.currentAppLogoutUrl.contains("?")){
+	public String buildRedirectToKi4soServer(){
+		StringBuffer sb = new StringBuffer(currentAppLogoutUrl);
+		if(currentAppLogoutUrl.contains("?")){
 			sb.append("&");
 		}
 		else{
 			sb.append("?");
 		}
 		sb.append("service=").append(logoutSuccessUrl);
+		logger.debug("logout and logoutSuccessUrl:"+sb.toString());
 		return sb.toString();
 	}
 
