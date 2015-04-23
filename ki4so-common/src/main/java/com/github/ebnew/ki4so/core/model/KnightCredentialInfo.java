e014d43d258c7512a2a6c1d5731bc88d52bb3d50
zhenglu <zhixuan204@sina.com>
2015-04-23 11:33:11 +0800

    delete doc image

--- ki4so-common/src/main/java/com/github/ebnew/ki4so/core/model/KnightCredentialInfo.java
+++ ki4so-common/src/main/java/com/github/ebnew/ki4so/core/model/KnightCredentialInfo.java
@@ -1,8 +1,21 @@
 package com.github.ebnew.ki4so.core.model;
 
+import java.io.Serializable;
+
 /**
+ * 加密凭证信息
  * @author zhenglu
  * @since 15/4/23
  */
-public class KnightCredentialInfo {
+public class KnightCredentialInfo implements Serializable{
+
+    private static final long serialVersionUID = -6546063621536994328L;
+    /**
+     *
+     */
+    private String appId;
+
+    private String userId;
+
+
 }
